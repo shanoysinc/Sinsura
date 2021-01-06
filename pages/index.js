795f7dd20a3c3eca8c72a3ef1c51e1084e51f9f7
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-06 09:50:51 -0800

    add company stories component to view

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import styles from "../styles/Home.module.scss";
 import NavBar from "../components/navbar/Navbar";
 import MainJumbotron from "../components/jumbotron/MainJumbotron";
+import CompanyStories from "../components/companyStories/CompanyStories";
+
 export default function Home() {
 	return (
 		<>
 			<NavBar />
 			<div className={styles.container}>
 				<MainJumbotron />
+				<CompanyStories />
 			</div>
 		</>
 	);
