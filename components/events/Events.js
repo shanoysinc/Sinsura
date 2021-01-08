e9e01da67ef62bc17e0403463ccd95319befe5ec
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-07 16:52:09 -0800

    make event cards link

--- components/events/Events.js
+++ components/events/Events.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/events/events.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 const Events = () => {
 	return (
@@ -25,13 +26,17 @@ const Events = () => {
 				</div>
 				<div className={styles.firstCol}>
 					<p className={styles.eventTitle}>Upcoming Events </p>
-					<Image
-						src="/svg/workshop.svg"
-						height="600"
-						layout="intrinsic"
-						width="400"
-						style={styles.upcomingImgSVG}
-					/>
+					<Link href="/">
+						<a>
+							<Image
+								src="/svg/workshop.svg"
+								height="600"
+								layout="intrinsic"
+								width="400"
+								style={styles.upcomingImgSVG}
+							/>
+						</a>
+					</Link>
 					<div className={styles.upcomingEvents}>
 						<p className={styles.gridParagraph}>
 							Asia's biggest GraphQL Conference is coming back in
@@ -42,13 +47,18 @@ const Events = () => {
 				</div>
 				<div className={styles.firstCol}>
 					<div className={styles.upcomingEvents}>
-						<Image
-							src="/svg/community-call.svg"
-							height="600"
-							width="400"
-							layout="intrinsic"
-							style={styles.upcomingImgSVG}
-						/>
+						<Link href="/">
+							<a>
+								<Image
+									src="/svg/community-call.svg"
+									height="600"
+									width="400"
+									layout="intrinsic"
+									style={styles.upcomingImgSVG}
+								/>
+							</a>
+						</Link>
+
 						<p className={styles.gridParagraph}>
 							Asia's biggest GraphQL Conference is coming back in
 							Feb 2021 and is now accepting proposals for talks &
