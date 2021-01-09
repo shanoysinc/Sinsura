4896f64248d1ded349a19f1d5cfdd2283f14bbf0
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-08 16:10:32 -0800

    add and style newsletter component

---
+++ components/newsLetter/NewsLetter.js
@@ -0,0 +1,22 @@
+import styles from "../../styles/newsLetter/newsLetter.module.scss";
+
+const NewsLetter = () => {
+	return (
+		<div className={styles.newsLetterContainer}>
+			<h3>Stay in the know</h3>
+			<p>
+				Sign up for full access to our product and community highlights,
+				new features, and occasional baby animal gifs! Oh, and we have a
+				strict no-spam rule. ✌️
+			</p>
+			<input type="text" placeholder="Your Email Address" />
+			<div className={styles.btnContainer}>
+				<a href="/">
+					<button className={styles.btn}>Subscribe</button>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default NewsLetter;
