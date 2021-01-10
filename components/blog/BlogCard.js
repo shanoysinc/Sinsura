65a3cc9fe0d646bae30d606b8469292bbe0efb5c
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-09 17:55:24 -0800

    optimize blog card for different screens

--- components/blog/BlogCard.js
+++ components/blog/BlogCard.js
@@ -3,10 +3,14 @@ import styles from "../../styles/blog/blogCard.module.scss";
 const BlogCard = ({ image, content }) => {
 	return (
 		<a href="/" className={styles.container}>
-			<img src={image} alt="news Icon" />
-			<p>
-				{content} <span> →</span>
-			</p>
+			<div className={styles.imgContainer}>
+				<img src={image} alt="news Icon" />
+			</div>
+			<div className={styles.paragraphContainer}>
+				<p>
+					{content} <span> →</span>
+				</p>
+			</div>
 		</a>
 	);
 };
--- components/features/Features.js
+++ components/features/Features.js
@@ -5,35 +5,42 @@ const Features = () => {
 	return (
 		<div className={styles.container}>
 			<h2>Hasura makes GraphQL easy</h2>
-			<FeatureItems
-				icon={"/svg/feature_icon_1.svg"}
-				title={"Instant Realtime GraphQL APIs"}
-				content={
-					"Point Hasura at your databases and instantly get a rich,realtime API instantly, without disrupting your existing apps."
-				}
-			/>
-			<FeatureItems
-				icon={"/svg/feature_icon_2.svg"}
-				title={"Built-in Granular Authorization"}
-				content={
-					"Hasura comes with a highly granular rule & role based authorization engine for your GraphQL APIs."
-				}
-			/>
-			<FeatureItems
-				icon={"/svg/feature_icon_3.svg"}
-				title={"Easily Integrate Business Logic"}
-				content={
-					"Extend your API by adding business logic in any language or framework & trigger workflows based on them."
-				}
-			/>
-			<BlogCard
-				image={"/svg/card_icon_1.svg"}
-				content={"new to GraphQl? Join our weekly demos"}
-			/>
-			<BlogCard
-				image={"/svg/card_icon_2.svg"}
-				content={"Hasura is the fastest GraphQL engine on the planet."}
-			/>
+			<div className={styles.itemContainer}>
+				<FeatureItems
+					icon={"/svg/feature_icon_1.svg"}
+					title={"Instant Realtime GraphQL APIs"}
+					content={
+						"Point Hasura at your databases and instantly get a rich,realtime API instantly, without disrupting your existing apps."
+					}
+				/>
+				<FeatureItems
+					icon={"/svg/feature_icon_2.svg"}
+					title={"Built-in Granular Authorization"}
+					content={
+						"Hasura comes with a highly granular rule & role based authorization engine for your GraphQL APIs."
+					}
+				/>
+				<FeatureItems
+					icon={"/svg/feature_icon_3.svg"}
+					title={"Easily Integrate Business Logic"}
+					content={
+						"Extend your API by adding business logic in any language or framework & trigger workflows based on them."
+					}
+				/>
+			</div>
+
+			<div className={styles.blogContainer}>
+				<BlogCard
+					image={"/svg/card_icon_1.svg"}
+					content={"new to GraphQl? Join our weekly demos"}
+				/>
+				<BlogCard
+					image={"/svg/card_icon_2.svg"}
+					content={
+						"Hasura is the fastest GraphQL engine on the planet."
+					}
+				/>
+			</div>
 		</div>
 	);
 };
