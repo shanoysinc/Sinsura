930ce27757b262e16f5d7a66d358b0c35e762704
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-09 19:43:43 -0800

    create a grid for items

--- components/Cloud Section/CloudSection.js
+++ components/Cloud Section/CloudSection.js
@@ -46,36 +46,38 @@ const CloudSection = () => {
 					</div>
 				</div>
 
-				<FeatureItems
-					icon="/svg/feature_icon_4.svg"
-					title="Fully Managed"
-					content="No-ops & auto-scalable: Hasura cloud doesn’t ask you to think about the number of instances, cores, memory, thresholds etc. Hook up your database and other services, and it just works."
-				/>
-				<FeatureItems
-					icon="/svg/feature_icon_5.svg"
-					title="Caching"
-					content="Hasura Cloud supports query caching as well as data caching by adding an @cached directive to your queries."
-				/>
-				<FeatureItems
-					icon="/svg/feature_icon_6.svg"
-					title="Monitoring & Analytics"
-					content="Hasura Cloud ships with monitoring and tracing features that help you understand database query performance and give you insight into what to do. Hasura Cloud has support for monitoring errors, web socket connections and subscriptions."
-				/>
+				<div className={styles.featureItemGrid}>
+					<FeatureItems
+						icon="/svg/feature_icon_4.svg"
+						title="Fully Managed"
+						content="No-ops & auto-scalable: Hasura cloud doesn’t ask you to think about the number of instances, cores, memory, thresholds etc. Hook up your database and other services, and it just works."
+					/>
+					<FeatureItems
+						icon="/svg/feature_icon_5.svg"
+						title="Caching"
+						content="Hasura Cloud supports query caching as well as data caching by adding an @cached directive to your queries."
+					/>
+					<FeatureItems
+						icon="/svg/feature_icon_6.svg"
+						title="Monitoring & Analytics"
+						content="Hasura Cloud ships with monitoring and tracing features that help you understand database query performance and give you insight into what to do. Hasura Cloud has support for monitoring errors, web socket connections and subscriptions."
+					/>
 
-				<FeatureItems
-					icon="/svg/feature_icon_7.svg"
-					title="Query Capture"
-					content="Hasura cloud captures queries and makes it available for regression testing so you know if any change you make affects the GraphQL queries you’re seeing in production. You can use the set of captured queries to create or update an allow list."
-				/>
-				<FeatureItems
-					icon="/svg/feature_icon_8.svg"
-					title="API Limiting"
-					content="Setup API rate limits and depth limits to prevent abuse/ DoS attacks on your GraphQL API. You can manage QoS with user identity & session aware configuration."
-				/>
-				<BlogCard
-					image="/svg/graphql.svg"
-					content="GraphQl in Production"
-				/>
+					<FeatureItems
+						icon="/svg/feature_icon_7.svg"
+						title="Query Capture"
+						content="Hasura cloud captures queries and makes it available for regression testing so you know if any change you make affects the GraphQL queries you’re seeing in production. You can use the set of captured queries to create or update an allow list."
+					/>
+					<FeatureItems
+						icon="/svg/feature_icon_8.svg"
+						title="API Limiting"
+						content="Setup API rate limits and depth limits to prevent abuse/ DoS attacks on your GraphQL API. You can manage QoS with user identity & session aware configuration."
+					/>
+					<BlogCard
+						image="/svg/graphql.svg"
+						content="GraphQl in Production"
+					/>
+				</div>
 			</div>
 		</div>
 	);
