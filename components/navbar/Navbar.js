94d1708261d3df1329b320891a8a09df3a8dc07a
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-10 17:00:20 -0800

    add navbar items

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,12 +1,46 @@
 import styles from "../../styles/navbar/NavBar.module.scss";
 import Logo from "../../public/svg/logo.svg";
-import HamburgerIcon from "../../public/svg/hamburgerIcon.svg";
-
+import Image from "next/image";
+import Link from "next/link";
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
-			<Logo />
-			<HamburgerIcon height={35} width={35} />
+			<div className={styles.navbarItems}>
+				<Logo />
+				<div className={styles.navbarList}>
+					<Link href="/">
+						<a className={styles.navLinks}>products</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>resources</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>pricing</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>blog</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>docs</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>contact sales</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinks}>login</a>
+					</Link>
+					<Link href="/">
+						<a className={styles.navLinkButton}>try hasura</a>
+					</Link>
+				</div>
+				<div className={styles.hamburgerIcon}>
+					<Image
+						height={35}
+						width={35}
+						src="/svg/hamburgerIcon.svg"
+					/>
+				</div>
+			</div>
 		</nav>
 	);
 };
