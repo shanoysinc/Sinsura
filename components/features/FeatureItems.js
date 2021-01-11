d5ad47989efe2f6150328473093d476a78f43f29
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-10 20:40:16 -0800

    change icon size

--- components/features/FeatureItems.js
+++ components/features/FeatureItems.js
@@ -1,8 +1,9 @@
 import styles from "../../styles/features/FeatureItems.module.scss";
+import Image from "next/image";
 const FeatureItems = ({ icon, title, content }) => {
 	return (
 		<div className={styles.container}>
-			<img src={icon} alt="feature icon" />
+			<Image src={icon} alt="feature icon" height={35} width={35} />
 			<h3>{title}</h3>
 			<p>{content}</p>
 		</div>
